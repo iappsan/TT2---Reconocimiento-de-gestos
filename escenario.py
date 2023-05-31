f18ee2c2a2de2ac4a32d369f7584f37c9ab27d1d
Ian Sanchez <iann.ipn9@gmail.com>
2023-05-31 00:41:26 -0600

    control de escenario actualizado

--- escenario.py
+++ escenario.py
@@ -1,5 +1,6 @@
 class Scene:
     
+    _FILE_ = ''
     _RAW_TEXT_ = []             # Texto crudo
     _SCENE_NAME_ = ''           # Nombre de escenario
     _SCENE_DATE_ = ''           # Fecha de creacion
@@ -7,45 +8,42 @@ class Scene:
 
     FILEPATH = ''               # Ruta de escenario
 
-    def sortGestures(lenght, RAW_DATA):          # Acomoda y normaliza las asociaciones en un arreglo
-        i = 2       # Porque en la posicion 2 comienzan las asociaciones
+    def sortGestures(self, len, RAW_DATA):    # Acomoda y normaliza las asociaciones en un arreglo
+        i = 2                           # Porque en la posicion 2 comienzan las asociaciones
         SORT_DATA = []
-        while i < lenght:
+        while i < len:
             SORT_DATA.insert(len(SORT_DATA),RAW_DATA[i].split(','))
             i += 1
         return SORT_DATA
 
-    def loadScene(args):
-        global _RAW_TEXT_
-        global _SCENE_NAME_
-        global _SCENE_DATE_
-        global _GEST_DICT_
+    def _init_(self, file):
 
         print("Leyendo escenario ...")
 
-        file = open (FILEPATH, 'r')
+        self._FILE_ = file
         CONTENIDO = file.readlines()
         for line in CONTENIDO:
-            _RAW_TEXT_.append(line.replace('\n',''))
+            self._RAW_TEXT_.append(line.replace('\n',''))
         file.close()
 
-        _SCENE_NAME_ = _RAW_TEXT_[0]         # Cargamos el titulo
-        _SCENE_DATE_ = _RAW_TEXT_[1]         # Cargamos la fecha
+        self._SCENE_NAME_ = self._RAW_TEXT_[0]         # Cargamos el titulo
+        self._SCENE_DATE_ = self._RAW_TEXT_[1]         # Cargamos la fecha
     
-        _GEST_DICT_ = sortGestures(len(_RAW_TEXT_), _RAW_TEXT_)
+        self._GEST_DICT_ = self.sortGestures(len(self._RAW_TEXT_), self._RAW_TEXT_)
 
-    def updateScene(opType, _DATA_):        # Se utiliza cada que movemos algo en la configuracion del escenario
-        global _GEST_DICT_                  # Args: 1:add, mod, del.  2:  
+    
 
+    def updateSceneGestures(self, opType, _DATA_):      # Se utiliza cada que movemos algo en la configuracion del escenario
+                                                # Args: 1:add, mod, del.  2:  
         if opType == 1:
             print ('Add')
-            _GEST_DICT_.insert(_DATA_)      # Agregamos al final del diccionario el nuevo gesto
+            self._GEST_DICT_.insert(_DATA_)      # Agregamos al final del diccionario el nuevo gesto
 
         elif opType == 2:
             print ('Mod')
             gestFound = False
 
-            for gest in _GEST_DICT_:
+            for gest in self._GEST_DICT_:
                 if not gestFound:
                     if gest[0] == _DATA_[0]:        # Si se encuentra el gesto que se busca actualizar
                         gest = _DATA_               # se actualiza el gesto, la accion y su link
@@ -56,30 +54,35 @@ class Scene:
             gestFound = False
             i = 0
 
-            for gest in _GEST_DICT_:
+            for gest in self._GEST_DICT_:
                 if not gestFound:
                     if gest[0] == _DATA_[0]:        # Si se encuentra el gesto se elimina segun el
-                        _GEST_DICT_.pop(i)          # indice en el que se encuentre
+                        self._GEST_DICT_.pop(i)     # indice en el que se encuentre
                     else:
                         i+=1
 
-    def saveScene():
-        global _RAW_TEXT_
-        global _SCENE_NAME_
-        global _SCENE_DATE_
-        global _GEST_DICT_
+    def separateLists(self,DATA):
+        newData = []
 
+        for line in DATA:
+            newLine = line[0]+','+line[1]+','+line[2]
+            newData.append(newLine)
+        
+        return newData
+
+    def saveScene(self):
         print("Creando archivo para escenario ...")
 
-        newFile = open('.\\Escenarios\\'+_SCENE_NAME_.replace(' ','')+'.txt','w')
-        newFile.write(_SCENE_NAME_)
-        newFile.write(_SCENE_DATE_)
-        newFile.writelines(_GEST_DICT_)
+        newFile = open('.\\Escenarios\\'+self._SCENE_NAME_.replace(' ','')+'.txt','w')
+        newFile.write(self._SCENE_NAME_)
+        newFile.write(self._SCENE_DATE_)
+        newFile.writelines(self.separateLists(self._GEST_DICT_))
         newFile.close()
 
         print('Listo')
 
+    def setFILEPATH(self, path):
+        self.FILEPATH = path
 
-    def setFILEPATH(path):
-        global FILEPATH
-        FILEPATH = path
+    def setSceneName(self, newName):
+        self._SCENE_NAME_ = newName
